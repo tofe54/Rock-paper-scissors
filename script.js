d5c72ad39dc02318a4c709621363d7a3308f7aa1
tofe54 <boggartripper@gmail.com>
2022-06-09 19:19:28 -0400

    Finish product to satisfactory functionality
    Game plays fully. Only bug is that it takes one extra click after reaching 5 points to announce win/lose.
    Need to add player and comp output, and add a play again button.

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 let playerCount = 0;
 let compCount = 0;
 
+const buttons = document.querySelectorAll('button')
 
 
-function play (computerSelection) {
-    let playerSelection = prompt("Rock, Paper, or Scissors: ");
-    
+function play (playerSelection) {
+   let computerSelection = computerPlay();
+   const results = document.querySelector('#results')
+   const pCount = document.querySelector("#playercount")
+   const comCount = document.querySelector("#compcount")
+
+
+   let result = " "
     if(playerSelection == 'rock' && computerSelection == 'paper') {
         compCount += 1;
-         alert("You lose, paper beats rock")
+         result = "You lose, paper beats rock" 
     }else if(playerSelection == 'rock' && computerSelection == 'scissors') {
         playerCount += 1;
-         alert("You win, rock beats scissors")
+         result = "You win, rock beats scissors"
     }else if(playerSelection == 'paper' && computerSelection == 'scissors') {
         compCount += 1;
-         alert("You lose, scissors beats paper")
+         result = "You lose, scissors beats paper"
     }else if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerCount += 1
-         alert("You win, paper beats rock")
+         result = "You win, paper beats rock"
     }else if(playerSelection == 'scissors' && computerSelection == 'paper'){
         playerCount += 1
-         alert("You win, scissors beats paper")
+         result = "You win, scissors beats paper"
     }else if(playerSelection == 'scissors' && computerSelection == 'rock') {
         compCount += 1;
-         alert("You lose, rock beats scissors")
+         result = "You lose, rock beats scissors"
     }else if (playerSelection === computerSelection){
-         alert("You tied try again")
+         result = "You tied try again"
     }else{
-        alert("Try again")
+        result = "Try again"
     }
 
+    return results.textContent = result, pCount.textContent = playerCount, comCount.textContent = compCount, console.log(computerSelection)
+    
 }
 
 
 function computerPlay() {
     let arr = ['rock', 'paper', 'scissors']
-    random = Math.floor(Math.random()* arr.length)
+    random = Math.floor(Math.random()* 3)
     return arr[random]
 }
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        play(computerPlay())
-        console.log(computerPlay())
-        console.log(playerCount)
-        console.log(compCount)
-     }
 
-   
-}
+const win_lose = document.querySelector('#win-lose')
 
-
-
-game();
-
-
-
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        if (playerCount >= 5 || compCount >= 5) {
+            if (playerCount == 5){
+                return win_lose.textContent = "You Win!!!"
+            } else if (compCount >= 5){
+                return win_lose.textContent= "You Lose!"
+            }
+            }
+        play(button.value)
+    })
+})
